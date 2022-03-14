5b0e21f1c9300c057af926f6b49651f60c0be807
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-03-14 15:55:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+mustache_x=0;
+mustache_y=0;
+
 function preload(){
+    mustache = loadImage("file:///C:/Users/cutes/OneDrive/Desktop/visual%20studio%20code/project-114/beard.jpg");
 }
 
 function setup(){
@@ -13,7 +17,8 @@ function setup(){
 }
 
 function draw(){
-    image(video,80,120,300,300);
+    image(video,80,120,300,300)
+    image(mustache,mustache_x,mustache_y,60,45);
 }
 
 function take_snapshot(){
@@ -28,6 +33,8 @@ function gotPose(results){
     if(results.length > 0){
         console.log(results);
         console.log("nose x: "+results[0].pose.nose.x);
+        mustache_x = results[0].pose.nose.x-30;
         console.log("nose y: "+results[0].pose.nose.y);
+        mustache_y = results[0].pose.nose.y;
     };
 }
