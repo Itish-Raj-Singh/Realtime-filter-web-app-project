1a45e14af0248a742cc4a3be8c56da05db2f3952
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-03-16 19:59:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function setup(){
     canvas = createCanvas(450,400);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,300);
     video.hide();
 
     poseNet = ml5.poseNet(video,modelLoaded);
