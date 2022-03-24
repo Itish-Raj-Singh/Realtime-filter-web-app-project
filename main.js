166a63b5ffbce0331a8e4ad81d9e66cf6c3b8d8c
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-03-24 11:25:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-mustache_x=0;
-mustache_y=0;
-
 function preload(){
-    mustache = loadImage("file:///C:/Users/cutes/OneDrive/Desktop/visual%20studio%20code/project-114/beard.jpg");
 }
 
 function setup(){
-    canvas = createCanvas(450,400);
+    canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
 
     poseNet = ml5.poseNet(video,modelLoaded);
@@ -16,8 +13,7 @@ function setup(){
 }
 
 function draw(){
-    image(video,80,120,300,300)
-    image(mustache,mustache_x,mustache_y,60,45);
+    image(video,0,0,300,300);
 }
 
 function take_snapshot(){
@@ -32,8 +28,6 @@ function gotPose(results){
     if(results.length > 0){
         console.log(results);
         console.log("nose x: "+results[0].pose.nose.x);
-        mustache_x = results[0].pose.nose.x-30;
         console.log("nose y: "+results[0].pose.nose.y);
-        mustache_y = results[0].pose.nose.y;
     };
 }
